e055a972f16c772253c669153f501a6a6251de72
Guy Sopher <guysopher@gmail.com>
2013-07-06 01:34:26 +0300

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wouldupay::Application.config.secret_token = '66ae70c6b8b813ddde762bb5c0980bcc7b44d33f35b2039da8ebbd3a495f9b45b0c9d8a0553fbe0f8d05aaece25dbcfade9811292da390e942d2bceaf87696d8'
